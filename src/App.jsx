aa65db6c6cee962f74da7b84b433e1ae1b3a9051
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-06-08 00:50:28 -0300

    UserInput Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
 function App() {
-  return <Header />;
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(event) {
+    const { id, value } = event.target;
+    setUserInput((prevInput) => ({
+      ...prevInput,
+      [id]: parseFloat(value),
+    }));
+  }
+  return (
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput} />
+    </>
+  );
 }
 
 export default App;
